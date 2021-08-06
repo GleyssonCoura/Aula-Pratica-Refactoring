71be9038ef47b1802bef3f177b1b4af834bbd238
GleyssonCoura <84857814+GleyssonCoura@users.noreply.github.com>
2021-08-06 19:57:45 -0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -37,6 +37,24 @@ public class Customer {
       return result;
    }
    
+   public String statement() {
+      Enumeration rentals = _rentals.elements();
+      String result = "Rental Record for " + getName() + "\n";
+      while (rentals.hasMoreElements()) {
+         Rental each = (Rental) rentals.nextElement();
+
+         // show figures for this rental
+         result += "\t" + each.getMovie().getTitle()+ "\t" +
+                  String.valueOf(each.getCharge()) + "\n";
+      }
+
+      // add footer lines
+      result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+      result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
+                     " frequent renter points";
+      return result;
+   }
+   
    public String htmlStatement() {
       Enumeration rentals = _rentals.elements();
       String result = "<H1>Rentals for <EM>" + getName() + "</EM></H1><P>\n";
