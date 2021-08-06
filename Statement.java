1bf5267b5f155834a35c07424bd33ef5bacf28d4
GleyssonCoura <84857814+GleyssonCoura@users.noreply.github.com>
2021-08-06 19:57:31 -0300

    Update Statement.java

--- Statement.java
+++ Statement.java
@@ -1,21 +1,4 @@
 public class Statement {
-      public String statement() {
-      Enumeration rentals = _rentals.elements();
-      String result = "Rental Record for " + getName() + "\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-
-         // show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-                  String.valueOf(each.getCharge()) + "\n";
-      }
-
-      // add footer lines
-      result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-      result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
-                     " frequent renter points";
-      return result;
-   }
-
+      
 }
 
