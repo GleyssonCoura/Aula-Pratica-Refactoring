9f36bf91598324f92464477c54d3a3c8cb11c57e
GleyssonCoura <84857814+GleyssonCoura@users.noreply.github.com>
2021-08-06 19:53:11 -0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -16,25 +16,7 @@ public class Customer {
    public String getName (){
       return _name;
    }
-  
-   public String statement() {
-      Enumeration rentals = _rentals.elements();
-      String result = "Rental Record for " + getName() + "\n";
-      while (rentals.hasMoreElements()) {
-         Rental each = (Rental) rentals.nextElement();
-
-         // show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-                  String.valueOf(each.getCharge()) + "\n";
-      }
-
-      // add footer lines
-      result +=  "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-      result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
-                     " frequent renter points";
-      return result;
-   }
-      
+       
    private double getTotalCharge() {
       double result = 0;
       Enumeration rentals = _rentals.elements();
