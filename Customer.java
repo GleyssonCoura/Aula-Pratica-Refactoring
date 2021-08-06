3ae6b05e44ffaf88f9de36a813374f193a9991a5
GleyssonCoura <84857814+GleyssonCoura@users.noreply.github.com>
2021-08-06 19:06:38 -0300

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -23,25 +23,8 @@ public class Customer {
      Enumeration rentals = _rentals.elements();
      String result = "Rental Record for " + getName() + "\n";
      while (rentals.hasMoreElements()) {
-        double thisAmount = 0;
+        double thisAmount = amountFor(each);;
         Rental each = (Rental) rentals.nextElement();
-
-        //determine amounts for each line
-        switch (each.getMovie().getPriceCode()) {
-           case Movie.REGULAR:
-              thisAmount += 2;
-              if (each.getDaysRented() > 2)
-                 thisAmount += (each.getDaysRented() - 2) * 1.5;
-              break;
-           case Movie.NEW_RELEASE:
-              thisAmount += each.getDaysRented() * 3;
-              break;
-           case Movie.CHILDRENS:
-              thisAmount += 1.5;
-              if (each.getDaysRented() > 3)
-                 thisAmount += (each.getDaysRented() - 3) * 1.5;
-               break;
-        }
 
         // add frequent renter points
         frequentRenterPoints ++;
@@ -61,4 +44,21 @@ public class Customer {
              " frequent renter points";
      return result;
    }
+   
+   //determine amounts for each line
+        switch (each.getMovie().getPriceCode()) {
+           case Movie.REGULAR:
+              thisAmount += 2;
+              if (each.getDaysRented() > 2)
+                 thisAmount += (each.getDaysRented() - 2) * 1.5;
+              break;
+           case Movie.NEW_RELEASE:
+              thisAmount += each.getDaysRented() * 3;
+              break;
+           case Movie.CHILDRENS:
+              thisAmount += 1.5;
+              if (each.getDaysRented() > 3)
+                 thisAmount += (each.getDaysRented() - 3) * 1.5;
+               break;
+        }
 }
